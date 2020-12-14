0df7dbec75e8b7b57459b6907528b4b7e6b37daa
John Gile <me@jgile.com>
2020-12-14 08:21:29 -0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <div>
+    <a href="assets/csv-sample.csv">Sample CSV</a>
+  </div>
+  <div>
+    <vue-csv-import v-model="mappedCsv" :fields="fields">
+      <div>
+        <vue-csv-toggle-headers></vue-csv-toggle-headers>
+      </div>
+      <div>
+        <vue-csv-errors></vue-csv-errors>
+      </div>
+      <div>
+        <vue-csv-input></vue-csv-input>
+      </div>
+      <div>
+        <vue-csv-map></vue-csv-map>
+      </div>
+    </vue-csv-import>
+  </div>
+</template>
+
+<script>
+import HelloWorldVue from "./components/HelloWorld.vue";
+export default {
+  name: "App",
+  data() {
+    return {
+      mappedCsv: [],
+      fields: {
+        name: { required: false, label: "Name" },
+        age: { required: true, label: "Age" },
+      },
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import { VueCsvImportPlugin } from "vue-csv-import";
+
+createApp(App).use(VueCsvImportPlugin).mount("#app");
